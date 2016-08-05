995de4c4b47326c468bb21c82bf0bed09d86a5a0
xiaxianlin <dreamtimefly@163.com>
2016-08-05 15:33:19 +0800

    fix

--- raphael/tools.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author xiaxianlin
- */
-!(function() {
-    var ele = document.getElementById('tools'),
-        w = ele.offsetWidth - 2,
-        h = ele.offsetHeight - 36,
-        paper = new Raphael('toolCanvas', w, h);
-}());

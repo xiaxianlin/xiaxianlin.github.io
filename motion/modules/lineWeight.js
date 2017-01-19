6690b1b8b620330f9bada92e24eaa897c614a34b
xiaxianlin <dreamtimefly@163.com>
2017-01-19 14:53:41 +0800

    add line weight

---
+++ motion/modules/lineWeight.js
@@ -0,0 +1,29 @@
+Modules.set('lineWeight', function(canvas, ctx) {
+    var centerX = canvas.width / 2,
+        centerY = canvas.height / 2,
+        raduis = 30,
+        weight = 1,
+        dir = 1,
+        minWeight = 0,
+        maxWeight = 5,
+        speed = 0.1;
+
+    function drawFrame() {
+        window.requestAnimationFrame(drawFrame, canvas);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, raduis, 0, 2 * Math.PI);
+        ctx.lineWidth = weight;
+        ctx.strokeStyle = '#fff';
+        ctx.stroke();
+        if (weight >= maxWeight) {
+            dir = -1;
+        } else if (weight <= minWeight) {
+            dir = 1;
+        }
+        weight += speed * dir;
+    }
+    return {
+        draw: drawFrame
+    }
+});

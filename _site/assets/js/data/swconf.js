ffc5d0d59cad404c499014d110dc51e9912715f7
xiaxianlin <xiaxianlin@bytedance.com>
2025-06-06 16:23:33 +0800

    site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1749198032',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
